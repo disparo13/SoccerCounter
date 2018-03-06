2fa9918c6970fc37015eb9dc561a3052a76d7fb7
disparo <mikstura@gmail.com>
2018-03-07 02:23:47 +0300

    First working version

--- app/src/main/java/su/mikheev/alex/soccercounter/MainActivity.java
+++ app/src/main/java/su/mikheev/alex/soccercounter/MainActivity.java
@@ -9,79 +9,97 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int warnTeamA = 0;
+    int penaltyTeamA = 0;
+    int warnTeamB = 0;
+    int penaltyTeamB = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        displayScoreForTeamA(scoreTeamA);
+        displayScoreForTeamB(scoreTeamB);
+        displayWarnsForTeamA(warnTeamA);
+        displayWarnsForTeamB(warnTeamB);
+        displayPenaltiesForTeamA(penaltyTeamA);
+        displayPenaltiesForTeamB(penaltyTeamB);
     }
 
     /**
-     * This method is called when the +3 button for Team A is clicked.
+     * This method is called when the score button for Team A is clicked.
      */
-    public void plusThreeForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     * This method is called when the +3 button for Team B is clicked.
-     */
-    public void plusThreeForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-
-    /**
-     * This method is called when the +2 button for team A is clicked.
-     */
-    public void plusTwoForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     * This method is called when the +2 button for Team B is clicked.
-     */
-    public void plusTwoForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-
-    /**
-     * This method is called when the Free Throw button for Team A is clicked.
-     */
-    public void freeThrowForTeamA(View view) {
+    public void scoreForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
+        displayScoreForTeamA(scoreTeamA);
     }
 
     /**
-     * This method is called when the Free Throw button for Team B is clicked.
+     * This method is called when the score button for Team B is clicked.
      */
-    public void freeThrowForTeamB(View view) {
+    public void scoreForTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
+        displayScoreForTeamB(scoreTeamB);
+    }
+
+
+    /**
+     * This method is called when the warning button for team A is clicked.
+     */
+    public void warnForTeamA(View view) {
+        warnTeamA = warnTeamA + 1;
+        displayWarnsForTeamA(warnTeamA);
     }
 
     /**
-     * Reset scores
+     * This method is called when the warning button for Team B is clicked.
+     */
+    public void warnForTeamB(View view) {
+        warnTeamB = warnTeamB + 1;
+        displayWarnsForTeamB(warnTeamB);
+    }
+
+
+    /**
+     * This method is called when the penalty button for Team A is clicked.
+     */
+    public void penaltyForTeamA(View view) {
+        penaltyTeamA = penaltyTeamA + 1;
+        displayPenaltiesForTeamA(penaltyTeamA);
+    }
+
+    /**
+     * This method is called when the penalty button for Team B is clicked.
+     */
+    public void penaltyForTeamB(View view) {
+        penaltyTeamB = penaltyTeamB + 1;
+        displayPenaltiesForTeamB(penaltyTeamB);
+    }
+
+    /**
+     * Reset scores and penalties
      */
     public void resetScores(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        warnTeamA = 0;
+        penaltyTeamA = 0;
+        warnTeamB = 0;
+        penaltyTeamB = 0;
+        displayScoreForTeamA(scoreTeamA);
+        displayScoreForTeamB(scoreTeamB);
+        displayWarnsForTeamA(warnTeamA);
+        displayWarnsForTeamB(warnTeamB);
+        displayPenaltiesForTeamA(penaltyTeamA);
+        displayPenaltiesForTeamB(penaltyTeamB);
+
     }
 
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
+    public void displayScoreForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
@@ -89,8 +107,41 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given score for Team B.
      */
-    public void displayForTeamB(int score) {
+    public void displayScoreForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    /**
+     * Display the given warns for Team A
+     */
+    public void displayWarnsForTeamA(int warnTeamA) {
+        TextView warnView = (TextView) findViewById(R.id.team_a_warns);
+        warnView.setText(String.valueOf(warnTeamA));
+    }
+
+    /**
+     * Display the given warns for Team B
+     */
+    public void displayWarnsForTeamB(int warnTeamB) {
+        TextView warnView = (TextView) findViewById(R.id.team_b_warns);
+        warnView.setText(String.valueOf(warnTeamB));
+    }
+
+    /**
+     * Display the given penalties for Team A
+     */
+    public void displayPenaltiesForTeamA(int penaltyTeamA) {
+        TextView penalView = (TextView) findViewById(R.id.team_a_penalties);
+        penalView.setText(String.valueOf(penaltyTeamA));
+    }
+
+    /**
+     * Display the given penalties for Team B
+     */
+    public void displayPenaltiesForTeamB(int penaltyTeamB) {
+        TextView penalView = (TextView) findViewById(R.id.team_b_penalties);
+        penalView.setText(String.valueOf(penaltyTeamB));
+    }
+
 }
